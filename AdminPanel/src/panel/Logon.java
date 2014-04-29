1ffb652b1c3156f586ee67889f21a394800d85e2
Denis <Den1-1@mail.ru>
2014-04-29 23:51:47 +0400

    Изменён родитель с DispatchAction на BaseAction

--- AdminPanel/src/panel/Logon.java
+++ AdminPanel/src/panel/Logon.java
@@ -3,27 +3,32 @@ package panel;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.actions.BaseAction;
 
-import javax.servlet.http.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Денис on 29.04.14.
  */
-public class Logon extends DispatchAction {
+public class Logon extends BaseAction {
+
     private static final String  FORWARD = "successful";
     private static final String  name = "admin";
     private static final String  password = "admin";
 
     @Override
-    protected String getMethodName(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response, String parameter) throws Exception {
-        return "logon";
-    }
-
-    public ActionForward logon(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         FormLogon logonData = (FormLogon) form;
         HttpSession httpSession = request.getSession();
-        response.addCookie(new Cookie("Logon", "Yes"));
-        return mapping.findForward(FORWARD);
+        if (logonData.getName().equals(name)  && logonData.getPassword().equals(password) ){
+            logonData.setStatus("Успех");
+            response.addCookie(new Cookie("Logon", "Yes"));
+            return mapping.findForward(FORWARD);
+        }
+        logonData.setStatus("Ошибка");
+        return mapping.findForward("error");
     }
 }
